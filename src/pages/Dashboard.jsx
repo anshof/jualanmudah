485128cbf837f60b5cfd95285d50d6168969e138
anashofiyakurniawati <shofiya@alterra.id>
2020-06-20 21:35:42 +0700

    get css

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 
 import { MDBBox } from "mdbreact";
-// import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar";
 
 class Dashboard extends Component {
   render() {
     return (
       <MDBBox>
-        {/* <Navbar style={{ position: "absolute", zIndex: "1" }} /> */}
+        <Navbar style={{ position: "absolute", zIndex: "1" }} />
         <MDBBox>
           <p>psdkjckndkcjnfkv</p>
         </MDBBox>
