d74b0f5ac7bd91cb85e09aedc51c81b9a2826d85
anashofiyakurniawati <shofiya@alterra.id>
2020-06-20 21:32:36 +0700

    first commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import { MDBBox } from "mdbreact";
+// import Navbar from "../components/Navbar";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <MDBBox>
+        {/* <Navbar style={{ position: "absolute", zIndex: "1" }} /> */}
+        <MDBBox>
+          <p>psdkjckndkcjnfkv</p>
+        </MDBBox>
+      </MDBBox>
+    );
+  }
+}
+export default Dashboard;
