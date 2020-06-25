5792fe2c01659de221ef8e1087f0d9edd932b0d4
anashofiyakurniawati <shofiya@alterra.id>
2020-06-25 19:29:10 +0700

    add content for email

---
+++ src/pages/NewBroadcast.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+import { MDBBox } from "mdbreact";
+
+import Navbar from "../components/Navbar";
+
+import "../css/style.css";
+import Recipient from "../components/Recipient";
+import ContentEmail from "../components/ContentEmail";
+
+class NewBroadcast extends Component {
+  render() {
+    return (
+      <MDBBox style={{ fontFamily: "Source Sans Pro" }}>
+        <Navbar />
+
+        <MDBBox className="py-4" style={{ backgroundColor: "#f7f7f7" }}>
+          <h3
+            className="text-left ml-5"
+            style={{ fontWeight: "600", color: "#192e35" }}
+          >
+            Create new broadcast
+          </h3>
+          {/* <Recipient /> */}
+          <ContentEmail />
+        </MDBBox>
+      </MDBBox>
+    );
+  }
+}
+export default NewBroadcast;
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 
-import Dashboard from "../pages/Dashboard";
+import NewBroadcast from "../pages/NewBroadcast";
 
 const MainRoutes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/" component={NewBroadcast} />
       </Switch>
     </BrowserRouter>
   );
