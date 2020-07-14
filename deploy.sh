95c9dff896e206182290412f9a793dd94dbf2322
adul11s <romli@alterra.id>
2020-07-14 21:42:03 +0700

    ganti port

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-st
 sudo docker stop slytherin-fe
 sudo docker rm slytherin-fe
 sudo docker rmi adul11/slytherin-fe:latest
-sudo docker run -d --name slytherin-fe -p 80:8443 adul11/slytherin-fe:latest
+sudo docker run -d --name slytherin-fe -p 8443:80 adul11/slytherin-fe:latest
